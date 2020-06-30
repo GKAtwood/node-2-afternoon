0a6fda2618d2d60e1ad3de6d56700cd2d9696d48
GKAtwood <gabrielle.k85@gmail.com>
2020-06-30 14:04:27 -0600

    commit

--- server/index.js
+++ server/index.js
@@ -1,26 +1,20 @@
- const express = require('express');
- const mc=require('./controller/messages_controller');
+const express = require('express');
+const mc = require('./controller')
 
- const app = express();
+const app = express();
 
- app.use(express.json());
+app.use(express.json());
 
- 
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
 
 
 
- const messagesBaseUrl = '/api/messages';
+const port = 3001;
 
- app.post(messagesBaseUrl, mc.create);
-
- app.get(messagesBaseUrl, mc.read);
-
- app.put(`${messagesBaseUrl}/:id`, mc.update);
-
- app.delete(`${messagesBaseUrl}/:id`, mc.delete);
-
- const port = 3001;
-
- app.listen(port. () => {
-     console.log('Server listening on port: ${port}');
- });
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}`);
+});
